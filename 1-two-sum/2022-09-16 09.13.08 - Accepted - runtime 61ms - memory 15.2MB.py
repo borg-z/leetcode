3a669b0180fb7813d5a0a1464c1907c1562768e9
borg-z <me@nikolay-z.top>
2024-02-27 14:07:43 +0300

    export

--- 1-two-sum/2022-09-16 09.13.08 - Accepted - runtime 61ms - memory 15.2MB.py
+++ 1-two-sum/2022-09-16 09.13.08 - Accepted - runtime 61ms - memory 15.2MB.py
@@ -1,9 +1,3 @@
-####
-# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-# You may assume that each input would have exactly one solution, and you may not use the same element twice.
-# You can return the answer in any order.
-####
-
 # class Solution:
 #     def twoSum(self, nums, target: int):
 #         d = dict()
@@ -33,6 +27,5 @@ class Solution:
         for i,num in enumerate(nums):
             needed = target - num
             if needed in d and d[needed] !=i:
-                print([i, d[needed]])
                 return [i, d[needed]]
 
